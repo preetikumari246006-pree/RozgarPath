fade376fe923cbad39b52f9833f04e6bccc28f07
PREETI KUMARI <preetikumari.246006@gmail.com>
2026-04-19 20:24:01 +0500

    Implement user greeting and career listing
    Added a function to welcome users and display careers based on their degree.

--- rozgarpath.py
+++ rozgarpath.py
@@ -1,1 +1,8 @@
-
+import data
+def rozgar_path(name, degree):
+    return f"Welcome {name}! We found careers for your {degree} degree!"
+user = input("Enter your name: ")
+degree = input("Enter your degree: ")
+print(rozgar_path(user, degree))
+for career in data.careers:
+    print(career["career_name"])
