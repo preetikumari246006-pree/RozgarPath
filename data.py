00d3a6888b216930535af5a64934ecbf6ad51776
PREETI KUMARI <preetikumari.246006@gmail.com>
2026-04-19 19:50:06 +0500

    Refactor career data structure to use a list
    Replaced rozgar_path dictionary with a careers list of dictionaries for better structure.

--- data.py
+++ data.py
@@ -1,5 +1,17 @@
-rozgar_path = {
-    "career_name": ["Agri Business Manager", "Real Estate"],
-    "skills_needed": ["Communication", "Excel", "Farm Management"],
-    "first_step": "Basic accounting"
-}
+careers = [
+    {
+        "career_name": "Agri Business Manager",
+        "skills_needed": ["Communication", "Excel", "Farm Management"],
+        "first_step": "Basic Accounting"
+    },
+    {
+        "career_name": "Real Estate",
+        "skills_needed": ["Communication", "Money management"],
+        "first_step": "Basic Business"
+    },
+    {
+        "career_name": "Agri Trader",
+        "skills_needed": ["Communication", "Money management", "Market Managment"],
+        "first_step": "Basic Trading"
+    }
+]
