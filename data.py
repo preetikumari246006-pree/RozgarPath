e98020f6df8c58ab1b288b09ef38c588a76f2742
PREETI KUMARI <preetikumari.246006@gmail.com>
2026-04-19 19:36:31 +0500

    Add rozgar_path dictionary with career details

--- data.py
+++ data.py
@@ -1,1 +1,5 @@
-
+rozgar_path = {
+    "career_name": ["Agri Business Manager", "Real Estate"],
+    "skills_needed": ["Communication", "Excel", "Farm Management"],
+    "first_step": "Basic accounting"
+}
