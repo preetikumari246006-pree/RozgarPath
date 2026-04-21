4d420079995775a0beaa42c66572ff42f1564bcd
PREETI KUMARI <preetikumari.246006@gmail.com>
2026-04-21 19:15:24 +0500

    Refactor rozgar_path function and improve output

--- rozgarpath.py
+++ rozgarpath.py
@@ -1,17 +1,13 @@
 import data
-
 def rozgar_path(name, degree):
-    ...
-
+    return f"Welcome {name}! We found careers for your {degree} degree!"
 user = input("Enter your name: ")
 degree = input("Enter your degree: ")
 print(rozgar_path(user, degree))
-
 count = 1
 for career in data.careers:
     print(count, career["career_name"])
     count += 1
-
 choice = int(input("Enter the number: "))
 if choice < 1 or choice > 3:
     print("This is invalid number! Please enter 1, 2 or 3")
