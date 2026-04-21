d1fe783f36e4cd1351c94f321326f4c030f388a1
PREETI KUMARI <preetikumari.246006@gmail.com>
2026-04-21 18:49:23 +0500

    Enhance career display with numbering for selection
    Refactor career listing to include numbering and improve user interaction.

--- rozgarpath.py
+++ rozgarpath.py
@@ -1,11 +1,21 @@
 import data
+
 def rozgar_path(name, degree):
     return f"Welcome {name}! We found careers for your {degree} degree!"
+
 user = input("Enter your name: ")
 degree = input("Enter your degree: ")
 print(rozgar_path(user, degree))
+
+# Show all careers
+count = 1
 for career in data.careers:
-    print("Career:", career["career_name"])
-    print("Skills:", ", ".join(career["skills_needed"]))
-    print("First Step:", career["first_step"])
-    print()
+    print(count, career["career_name"])
+    count += 1
+
+# User picks career
+choice = int(input("Enter the number: "))
+selected = data.careers[choice - 1]
+print("Career:", selected["career_name"])
+print("Skills:", ", ".join(selected["skills_needed"]))
+print("First Step:", selected["first_step"])
