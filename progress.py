a6d2c54f2aacce3b611020993c9eac0440cdd6cf
PREETI KUMARI <preetikumari.246006@gmail.com>
2026-04-22 19:42:26 +0500

    Refactor save_progress function for simplicity

--- progress.py
+++ progress.py
@@ -1,16 +1,7 @@
 def save_progress(name, degree, career):
     file = open("progress.txt", "w")
-    file.write("Name: " + name + "\n")
-    file.write("Degree: " + degree + "\n")
-    file.write("Career: " + career + "\n")
+    file.write(name + "\n")
+    file.write(degree + "\n")
+    file.write(career + "\n")
     file.close()
-    print("Progress Saved! ✅")
-
-def load_progress():
-    try:
-        file = open("progress.txt", "r")
-        data = file.read()
-        file.close()
-        return data
-    except:
-        return "No progress found!"
+    print("✅ Progress Saved!")
