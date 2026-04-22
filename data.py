55d2fd6ba7499541d0f290b412a06a67697f0ade
PREETI KUMARI <preetikumari.246006@gmail.com>
2026-04-22 19:41:49 +0500

    Enhance career data with additional resources
    Added resources for each career entry in data.py.

--- data.py
+++ data.py
@@ -2,16 +2,28 @@ careers = [
     {
         "career_name": "Agri Business Manager",
         "skills_needed": ["Communication", "Excel", "Farm Management"],
-        "first_step": "Basic Accounting"
+        "first_step": "Basic Accounting",
+        "resources": [
+            "Khan Academy - khanacademy.org",
+            "Alison Free Courses - alison.com"
+        ]
     },
     {
         "career_name": "Real Estate",
-        "skills_needed": ["Communication", "Money management"],
-        "first_step": "Basic Business"
+        "skills_needed": ["Communication", "Money Management"],
+        "first_step": "Basic Business",
+        "resources": [
+            "Coursera - coursera.org",
+            "YouTube - youtube.com"
+        ]
     },
     {
         "career_name": "Agri Trader",
-        "skills_needed": ["Communication", "Money management", "Market Managment"],
-        "first_step": "Basic Trading"
+        "skills_needed": ["Communication", "Money Management", "Market Management"],
+        "first_step": "Basic Trading",
+        "resources": [
+            "Google Skillshop - skillshop.google.com",
+            "Alison Free Courses - alison.com"
+        ]
     }
 ]
