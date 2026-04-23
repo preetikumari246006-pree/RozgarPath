5395c5d338f457ea298eada2742354549b3234ea
PREETI KUMARI <preetikumari.246006@gmail.com>
2026-04-23 20:22:20 +0500

    Remove save_progress function from progress.py

--- progress.py
+++ progress.py
@@ -1,7 +1,1 @@
-def save_progress(name, degree, career):
-    file = open("progress.txt", "w")
-    file.write(name + "\n")
-    file.write(degree + "\n")
-    file.write(career + "\n")
-    file.close()
-    print("✅ Progress Saved!")
+
