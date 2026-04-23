cc9c7cbdeddd543fed36b412a05519b4d72654ec
PREETI KUMARI <preetikumari.246006@gmail.com>
2026-04-23 20:22:37 +0500

    Implement save_progress function
    Added a function to save user progress to a file.

--- progress.py
+++ progress.py
@@ -1,1 +1,7 @@
-
+def save_progress(name, degree, career):
+    file = open("progress.txt", "w")
+    file.write(name + "\n")
+    file.write(degree + "\n")
+    file.write(career + "\n")
+    file.close()
+    print("✅ Progress Saved!")
